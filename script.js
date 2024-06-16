95b7fb882b4ca40bc40e7005a98455badc308bfb
GetnetAm <getnetamsalu2119@gmail.com>
2024-06-16 15:23:03 -0700

    design index for this portfolio

---
+++ script.js
@@ -0,0 +1,18 @@
+// page when clik next or previes button
+
+
+// const pageTurnBtn= document.querySelectorAll('.nextprev-btn');
+
+// pageTurnBtn.forEach((el, index)=>{
+//     el.onClick=()=>{
+//         const pageTurnId= el.getAttribute('data-page');
+//         const pageTurn=document.getElementById(pageTurnId);
+
+//         if(pageTurn.classList.contains('turn')){
+//             pageTurn.classList.remove('turn');
+//         }
+//         else{
+//             pageTurn.classList.add('turn');
+//         }
+//     }
+// })
